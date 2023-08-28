12b6a24d91b389f0f75c6c37f4b6eeef190fca31
Mehran Abdi <mehranabdi13793@gmail.com>
2023-08-28 14:05:42 +0330

    make authentication repository and db

---
+++ eCommerce.Repository/AuthRoles.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Repository.Authentication
+{
+    public static class AuthRoles
+    {
+        public static List<IdentityRole> IdentityRoles = new List<IdentityRole>()
+        {
+            new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                Name = "customer",
+                NormalizedName = "CUSTOMER"
+            }
+            ,
+            new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                Name = "admin",
+                NormalizedName = "ADMIN"
+            }
+            ,new IdentityRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+                Name = "seller",
+                NormalizedName = "SELLER"
+            }
+        };
+    }
+}
---
+++ eCommerce.Repository/eCommerceAuthDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Framework.Authentication.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Repository.Authentication
+{
+    public class eCommerceAuthDbContext : AuthenticationDbContext
+    {
+        public eCommerceAuthDbContext(DbContextOptions options) : base(options, AuthRoles.IdentityRoles)
+        {
+        }
+    }
+}
