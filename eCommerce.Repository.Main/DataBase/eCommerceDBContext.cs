41478e950556f60d5f0c095d2be16923a4ff1dc2
Mehran Abdi <mehranabdi13793@gmail.com>
2023-08-28 14:06:00 +0330

    create main DbContext

---
+++ eCommerce.Repository.Main/DataBase/eCommerceDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Framework.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Repository.Main.DataBase
+{
+    public class eCommerceDBContext : DataBaseContext
+    {
+        public eCommerceDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
