82c5dc06e553478b0c5c1af8ed04f038199fb54c
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-11 22:12:10 +0330

    Add VerifyUserEmail command and handler

---
+++ eCommerce.Service/User/Handlers/VerifyUserEmailHandler.cs
@@ -0,0 +1,25 @@
+﻿using eCommerce.Service.User.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Service.User.Handlers
+{
+    public class VerifyUserEmailHandler : IRequestHandler<VerifyUserEmailCommand, IdentityResult>
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        public VerifyUserEmailHandler(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        public async Task<IdentityResult> Handle(VerifyUserEmailCommand request, CancellationToken cancellationToken)
+        {
+            var user = await userManager.FindByIdAsync(request.UserId);
+            return await userManager.ConfirmEmailAsync(user, request.Token);
+        }
+    }
+}
