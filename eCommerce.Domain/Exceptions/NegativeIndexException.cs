91f8116daa4808d44466a8cbb0b6f66d7d3dbca2
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-12 20:41:45 +0330

    Add category exceptions

---
+++ eCommerce.Domain/Exceptions/NegativeIndexException.cs
@@ -0,0 +1,10 @@
+﻿using Framework.Domain;
+using System.Runtime.Serialization;
+
+namespace eCommerce.Domain.Exceptions
+{
+    public class NegativeIndexException : DomainException
+    {
+        public override string Message => ExceptionMessages.NegativeIndexExceptionMessage;
+    }
+}
