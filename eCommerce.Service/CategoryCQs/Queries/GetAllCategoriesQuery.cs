2320ae6011c899d3dff39d1f6fc27d90744f0354
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-12 20:43:22 +0330

    Add category queries

---
+++ eCommerce.Service/CategoryCQs/Queries/GetAllCategoriesQuery.cs
@@ -0,0 +1,11 @@
+﻿using eCommerce.Domain.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Service.CategoryCQs.Queries;
+
+public record GetAllCategoriesQuery: IRequest<List<Category>>;
