d1ca51f2ed5d58aa38218d493694efb376db523d
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-12 20:44:25 +0330

    Add user queries

---
+++ eCommerce.Service/UserCQs/Queries/SignUserInQuery.cs
@@ -0,0 +1,11 @@
+﻿using eCommerce.Service.Contracts.DTO;
+using Framework.Authentication.Service;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Service.User.Queries;
+public record SignUserInQuery(SignUpDTO Dto) : IRequest<SignInInformationDTO>;
