9f2a25915670042208096168ee16c1ab374bde38
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-12 20:44:14 +0330

    Add user handlers

---
+++ eCommerce.Service/UserCQs/Handlers/SendPasswordResetEmailHandler.cs
@@ -0,0 +1,32 @@
+﻿using eCommerce.Service.User.Queries;
+using Framework.Notification;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using NETCore.MailKit.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Service.User.Handlers;
+
+public class SendPasswordResetEmailHandler : IRequestHandler<SendPasswordResetEmailCommand, Task>
+{
+    private readonly UserManager<IdentityUser> userManager;
+    private readonly EmailServices emailServices;
+    public SendPasswordResetEmailHandler(UserManager<IdentityUser> userManager , IEmailService emailService)
+    {
+        this.userManager = userManager;
+        emailServices = new EmailServices(emailService);
+    }
+    public async Task<Task> Handle(SendPasswordResetEmailCommand request, CancellationToken cancellationToken)
+    {
+        var user = await userManager.FindByEmailAsync(request.Email);
+
+        var token = await userManager.GeneratePasswordResetTokenAsync(user);
+
+        return emailServices.SendPasswordResetEmail(request.Email, token);
+    }
+}
