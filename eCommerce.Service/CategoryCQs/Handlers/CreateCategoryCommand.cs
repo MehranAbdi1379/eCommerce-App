8ac8737e55de29aad7709648a92b1cc0176e1acc
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-12 20:42:57 +0330

    Add category commands

---
+++ eCommerce.Service/CategoryCQs/Handlers/CreateCategoryCommand.cs
@@ -0,0 +1,29 @@
+﻿using eCommerce.Domain.Models;
+using eCommerce.Repository.Main;
+using eCommerce.Service.CategoryCQs.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Service.CategoryCQs.Handlers
+{
+    public class CreateCategoryHandler : IRequestHandler<CreateCategoryCommand, Category>
+    {
+        private readonly ICategoryRepository categoryRepository;
+        public CreateCategoryHandler(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public Task<Category> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = new Category(request.dto.Title);
+            categoryRepository.Create(category);
+            categoryRepository.Save();
+            return Task.FromResult(category);
+        }
+    }
+}
