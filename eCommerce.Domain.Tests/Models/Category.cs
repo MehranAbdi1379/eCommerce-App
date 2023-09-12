e77ee64935e24a1ad5e8532773afec6ce4d816bb
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-12 20:41:33 +0330

    Add category model and test

---
+++ eCommerce.Domain.Tests/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Domain.Tests.Models
+{
+    [TestClass]
+    public class Category
+    {
+        [TestMethod]
+        public void SetTitle_Retrieve()
+        {
+            Category category = new();
+        }
+    }
+}
---
+++ eCommerce.Domain/Models/Category.cs
@@ -0,0 +1,38 @@
+﻿using eCommerce.Domain.Exceptions;
+using eCommerce.Repository.Main;
+using Framework.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Domain.Models
+{
+    public class Category : BaseEntity
+    {
+        public Category(string title)
+        {
+            SetTitle(title);
+        }
+
+        public void SetParentCategoryId(Guid parentCategoryId, ICategoryRepository categoryRespository)
+        {
+            if (!categoryRespository.IsExist(parentCategoryId))
+                throw new CategoryNotExistException();
+            ParentCategoryId = parentCategoryId;
+            Index = categoryRespository.GetById(parentCategoryId).Index + 1;
+        }
+
+        public void SetTitle(string title)
+        {
+            if (title == null || title == "")
+                throw new ArgumentNullException("title");
+            Title = title;
+        }
+
+        public string Title { get; private set; }
+        public Guid ParentCategoryId { get; private set; }
+        public int Index { get; private set; } = 0;
+    }
+}
