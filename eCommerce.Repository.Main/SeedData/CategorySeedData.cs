fe352e9d518d3682f76fd4a4a13dbfb36443e03a
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-19 22:07:09 +0330

    Fix category seed data

--- eCommerce.Repository.Main/SeedData/CategorySeedData.cs
+++ eCommerce.Repository.Main/SeedData/CategorySeedData.cs
@@ -1,4 +1,5 @@
 ﻿using Bogus;
+using Bogus.DataSets;
 using eCommerce.Domain.Models;
 using eCommerce.Repository.Main.DataBase;
 using Microsoft.EntityFrameworkCore;
@@ -24,29 +25,34 @@ namespace eCommerce.Repository.Main.SeedData
             var categoryFaker = new Faker<Category>()
             .RuleFor(c => c.Title, f => f.Commerce.Categories(1)[0]);
 
-            var faker = new Faker();
-
             var categories = categoryFaker.Generate(20);
 
-            for (int i = 0; i < categories.Count; i++)
+            for (int index = 0; index < categories.Count; index++)
             {
-                var category = categories[i];
-                if (categories.Count > 0 && category.Id != categories[0].Id)
-                {
-                    if (!faker.Random.Bool(0.25f))
-                    {
-                        Guid parentId;
-                        do
-                        {
-                            int index = faker.Random.Int(0, i);
-                            parentId = categories[index].Id;
-                        } while (parentId == category.Id);
-                        var categoryRepository = new CategoryRespository(context);
-                        category.SetParentCategoryId(parentId, categoryRepository);
-                    }
-                }
-                context.Categories.Add(category);
+                SeedData_PopulateParentIds(categories , index);
+
+                context.Categories.Add(categories[index]);
                 context.SaveChanges();
+            }
+        }
+
+        private void SeedData_PopulateParentIds(List<Category> categories , int categoryIndex)
+        {
+            var faker = new Faker();
+            var category = categories[categoryIndex];
+            if (categories.Count > 0 && category.Id != categories[0].Id)
+            {
+                if (!faker.Random.Bool(0.25f))
+                {
+                    Guid parentId;
+                    do
+                    {
+                        int index = faker.Random.Int(0, categoryIndex);
+                        parentId = categories[index].Id;
+                    } while (parentId == category.Id);
+                    var categoryRepository = new CategoryRespository(context);
+                    category.SetParentCategoryId(parentId, categoryRepository);
+                }
             }
         }
     }
