dded3cdd7c96ded98b04c18867b1d3058d953ac8
Mehran Abdi <mehranabdi13793@gmail.com>
2023-11-15 09:35:59 +0330

    Create product model

---
+++ eCommerce.Domain/Models/Product.cs
@@ -0,0 +1,86 @@
+﻿using eCommerce.Domain.Exceptions;
+using eCommerce.Domain.Exceptions.Category;
+using eCommerce.Domain.Exceptions.Product;
+using eCommerce.Domain.Repositories;
+using Framework.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Domain.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Title { get; private set; }
+        public Guid CategoryId { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; } = 0;
+        public string Description { get; private set; }
+        public List<string> PhotoURLs { get; private set; }
+
+        public Product(string title, 
+            string description, 
+            List<string> photoURLs, 
+            decimal price, 
+            int stock,
+            Guid categoryId
+            ,ICategoryRepository categoryRepository
+            ,IProductRepository repository)
+        {
+            SetTitle(title,repository);
+            SetDescription(description);
+            SetPhotoUrls(photoURLs);
+            SetPrice(price);
+            SetCategoryId(categoryId, categoryRepository);
+            SetStock(stock);
+        }
+
+        public void SetCategoryId(Guid categoryId, ICategoryRepository categoryRepository)
+        {
+            if (!categoryRepository.IsExist(categoryId))
+                throw new CategoryNotExistException();
+            if (categoryRepository.GetSubCategoriesByParentId(categoryId).Count != 0)
+                throw new ProductCategoryWithSubCategoryException();
+            CategoryId = categoryId;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price <= 0)
+                throw new ProductPriceNegativeOrZeroException();
+            Price = price;
+        }
+
+        public void SetPhotoUrls(List<string> photoURLs)
+        {
+            if (photoURLs.Count == 0)
+                throw new ProductPhotoCountZeroException();
+            PhotoURLs = photoURLs;
+        }
+
+        public void SetDescription(string description)
+        {
+            if (description.Length < 10)
+                throw new ProductDescriptionShorterThanTenLettersException();
+            Description = description;
+        }
+
+        public void SetTitle(string title, IProductRepository repository)
+        {
+            if (repository.TitleExists(title))
+                throw new ProductTitleExistException();
+            if (title.Length < 5)
+                throw new ProductTitleShortenThanFiveLettersException();
+            Title = title;
+        }
+
+        public void SetStock(int stock)
+        {
+            if (stock < 0)
+                throw new ProductStockNegativeException();
+            Stock = stock;
+        }
+    }
+}
