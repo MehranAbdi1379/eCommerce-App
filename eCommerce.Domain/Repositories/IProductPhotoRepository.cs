a8f96766769288cb899964a778bb7c487400e69c
Mehran Abdi <mehranabdi13793@gmail.com>
2023-11-18 11:01:04 +0330

    Add product photo repository

---
+++ eCommerce.Domain/Repositories/IProductPhotoRepository.cs
@@ -0,0 +1,9 @@
+﻿using eCommerce.Domain.Models;
+using Framework.Repository;
+
+namespace eCommerce.Domain.Repositories;
+
+public interface IProductPhotoRepository: IBaseRepository<ProductPhoto>
+{
+
+}
---
+++ eCommerce.Repository.Main/ProductPhotoRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerce.Domain.Models;
+using eCommerce.Domain.Repositories;
+using eCommerce.Repository.Main.DataBase;
+using Framework.Repository;
+
+namespace eCommerce.Repository.Main;
+
+public class ProductPhotoRepository: BaseRepository<ProductPhoto>, IProductPhotoRepository
+{
+    public ProductPhotoRepository(eCommerceDBContext context) : base(context)
+    {
+    }
+}
